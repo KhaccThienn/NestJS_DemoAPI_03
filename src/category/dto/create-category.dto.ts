1c1f76ef31604ae46eb9ebe1260c3059589fb518
Lê Khắc Thiện <muzankibut112@gmail.com>
2023-04-24 11:33:00 +0700

    Update a little

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -2,14 +2,10 @@
 import { Expose } from 'class-transformer';
 import { IsString } from 'class-validator';
 import { IsNotEmpty } from 'class-validator';
-import { IsUniqueCate } from 'src/validator/IsUnique.validator';
 export default class CreateCategoryDTO {
   @IsNotEmpty()
   @IsString()
   @Expose()
-  @IsUniqueCate({
-    message: 'This Category Already Exists',
-  })
   name: string;
 
   @IsNotEmpty()
